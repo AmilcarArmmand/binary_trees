0abe067c3a739a1e39c7967ffc43ab64269fae18
Amilcar Armmand <armmand@gmail.com>
2020-07-22 19:02:33 +0000

    added task 9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - a function deletes an entire binary tree
+ * @tree: a pointer to the root node of the tree to delete
+ *
+ * Return: no return data type
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left;
+	size_t right;
+
+	left = 0;
+	right = 0;
+
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
+	left = 1 + binary_tree_height(tree->left);
+	right = 1 + binary_tree_height(tree->right);
+
+	return (left < right ? left : right);
+}
