2d4737fd4645eaf2bd649a02b9e6c8573d351aee
Amilcar Armmand <armmand@gmail.com>
2020-07-22 21:28:49 +0000

    added if statement to measure along the edge of the longest path from the node to a leaf

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,11 +14,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	left = 0;
 	right = 0;
 
-	if (tree == NULL || \
-	    ((tree != NULL) && (tree->left == NULL && tree->right == NULL)))
-		return (0);
-	left = 1 + binary_tree_height(tree->left);
-	right = 1 + binary_tree_height(tree->right);
-
-	return (left < right ? left : right);
+	if (tree)
+	{
+		if (tree->left == NULL && tree->right == NULL)
+			return (0);
+		left = 1 + binary_tree_height(tree->left);
+		right = 1 + binary_tree_height(tree->right);
+		return (left < right ? left : right);
+	}
+	return (0);
 }
