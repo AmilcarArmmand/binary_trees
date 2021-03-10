2af09c5861f27fbbef8daec8e47267244c9cfa68
AmilcarArmmand <armmand@gmail.com>
2021-03-10 11:15:44 +0000

    refactored to test all nodes recursively

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -4,17 +4,21 @@
  * binary_tree_is_full - checks if a binary tree is full
  * @tree: a pointer to the root node of the tree to check
  *
- * Return: 1 if tree is FULL, otherwise O
+ * Return: 1 if tree is FULL, otherwise O and if tree is NULL.
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int full;
-
-	full = 0;
-
 	if (tree == NULL)
-		full = 0;
+		return (0);
 
+	/* base case of node with 0 leaves is FULL */
+	if (!(tree->left) && !(tree->right))
+		return (1);
 
-	return (full);
+	/* call function to test UNION of both leaf nodes being FULL */
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) &&
+			binary_tree_is_full(tree->right));
+
+	return (0);
 }
