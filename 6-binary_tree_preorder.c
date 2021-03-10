4d6ec4429f82e11a73f7ba8780833d9ace877db2
AmilcarArmmand <armmand@gmail.com>
2021-03-10 03:25:49 +0000

    updated description, removed comment in line 12

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -3,13 +3,12 @@
 /**
  * binary_tree_preorder - a function that pre-order traverses a binary tree
  * @tree: a pointer to the root node of the tree to traverse
- * @func: a pointer to a function to call for each node
+ * @func: a pointer to a function to call for each node, value passed as param
  *
- * Return: no return data type
+ * Return: No data type returned.  If tree or func is NULL do nothing.
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-/* if the binary tree exists, delete recursively, left then right */
 	if (tree && func)
 	{
 		func(tree->n);
